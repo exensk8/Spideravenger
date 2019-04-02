8fad546a83cf952c7d834d8effcec8fa7cf4d501
Luis <luis@Luis-MacBook-Pro-6.local>
2019-04-01 21:52:11 -0500

    add validate class

--- index.js
+++ index.js
@@ -5,21 +5,16 @@ const accountSid = 'AC751b816484c258227b97785b8a004d5a';
 const authToken = '1c33d6b493ce2a91d6fef2db92002f0b';
 const client = require('twilio')(accountSid, authToken);
 
-(async () => {
+async function searchMovie() {
+  console.log('Run function')
   const url = 'https://www.cineplanet.com.pe/peliculas/avengers-end-game';
-  const text = 'No hay funciones programadas para esta película';
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
 
   try {
-    await page.waitForFunction(
-      text => document.querySelector('body').innerText.includes(text),
-      {},
-      text
-    );
-  } catch(e) {
-    console.log(`The text "${text}" was not found on the page`);
+    await page.waitForSelector('.film-detail-showtimes--title');
+    console.log("Comprar pelicula")
     client.messages.create(
         {
           to: '+51962281080',
@@ -31,9 +26,13 @@ const client = require('twilio')(accountSid, authToken);
           console.log(message.sid);
         }
       );
+    await page.screenshot({path: 'page.png', fullPage:true});
+  } catch(e) {
+    console.log("Pelicula no esta disponible")
+    console.log("error detail ",e)
+
   }
-
-  await page.screenshot({path: 'page.png', fullPage:true});
   await browser.close();
-
-})();
+}
+searchMovie()
+setInterval(searchMovie, 180000);
